be7ae352bb86f67ff56d8e5ebbb632d47edddd7d
Ken Everett <ken.everett@gmail.com>
2016-05-29 12:30:53 -0700

    adding a place for images/json files and script to symlink it

---
+++ images/linkit.sh
@@ -0,0 +1,4 @@
+rm ../app/public/images/$1.jpg
+ln -s /Users/ken.everett/dev2/background/images/$1.jpg ../app/public/images/$1.jpg
+rm ../app/source.json
+ln -s /Users/ken.everett/dev2/background/images/$1.json ../app/source.json
