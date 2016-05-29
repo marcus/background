390c8b470d539f82a970dbd385f41e3eef9924bf
Ken Everett <ken.everett@gmail.com>
2016-05-29 12:47:16 -0700

    use dynamic pwd for linkit

--- images/linkit.sh
+++ images/linkit.sh
@@ -1,4 +1,4 @@
 rm ../app/public/images/$1.jpg
-ln -s /Users/ken.everett/dev2/background/images/$1.jpg ../app/public/images/$1.jpg
+ln -s `pwd`/$1.jpg ../app/public/images/$1.jpg
 rm ../app/source.json
-ln -s /Users/ken.everett/dev2/background/images/$1.json ../app/source.json
+ln -s `pwd`/$1.json ../app/source.json
