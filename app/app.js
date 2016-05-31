25a55f9cf2c7e160c79152865599ea6cb10e6bf4
Marcus Vorwaller <marcus@vorwaller.net>
2016-05-31 08:05:40 -0700

    Server accepts posts

--- app/app.js
+++ app/app.js
@@ -2,6 +2,12 @@ var url = require('url');
 var path = require('path');
 var fs = require('fs');
 var screenshot = require('electron-screenshot-service');
+var Promise = require('bluebird');
+var uuid = require('uuid');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var writeFile = Promise.promisify(fs.writeFile, fs);
 
 if(process.env.NODE_ENV !== 'production') {
   var webpack = require('webpack');
@@ -28,6 +34,10 @@ var app = express();
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json({limit: '1mb'})); // parse application/json
+app.use(methodOverride()); //  use HTTP verbs like PUT or DELETE where client doesn't support it.
+app.use(bodyParser.urlencoded({extended: false, limit: '1mb'}));
+
 // proxy the request for static assets in dev mode
 if(process.env.NODE_ENV !== 'production') {
   var proxy = require('proxy-middleware');
@@ -35,28 +45,46 @@ if(process.env.NODE_ENV !== 'production') {
 }
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Background', source: require('./source.json')});
+  var sourceFile = req.param('blueprint') || 'source.json';
+  console.log("Source", req.param('blueprint'));
+  try {
+    var source = require('./blueprints/' + sourceFile);
+  } catch (err) {
+    return res.status(400).send({message: err.message});
+  }
+  return res.render('index', { title: 'Background', source: source});
 });
 
 app.post('/print', function (req, res) {
-  // TODO - write the post to source.json
-  screenshot({
-    url : 'http://localhost:3000',
-    width : 1024,
-    height : 768
-  })
-  .then(function(img){
-    fs.writeFile('./quote.png', img.data, function(err){
-      if(err) {
-        console.log("There was a problem", err);
-      } else {
-        console.log("Done.");
-      }
+  var fileName = uuid.v4();
+
+  console.log("Writing body", req.body);
+  try{
+    console.log(req.body.quotation);
+  } catch(err) {
+    return res.status(400).send({message: "Can't parse body. " + err.message});
+  }
+  writeFile('./blueprints/' + fileName + '.json', JSON.stringify(req.body))
+    .then(function() {
+      console.log("Taking screenshot");
+      return screenshot({
+        url : 'http://localhost:3000?blueprint=' + fileName + '.json',
+        width : 1024,
+        height : 768
+      });
+    })
+    .then(function(img) {
+      return writeFile('./public/quotes/' + fileName + '.jpg', img.data);
+    })
+    .then(function() {
       screenshot.close();
-    });
-  }).then(function() {
-    res.render('index', { title: 'Background', source: req.body});
-  });
+      return res.status(200).send({message: 'Rendered ' + fileName});
+    })
+    .catch(function(err) {
+      console.log("There was a problem with the request", err);
+      return res.status(400).send({message: err.message});
+    })
+    ;
 });
 
 app.listen(3000, function () {
--- server2/convert.rb
+++
@@ -1,4 +0,0 @@
-require 'imgkit'
-
-kit = IMGKit.new('http://localhost:8080')
-kit.to_file('file.png')
